625ca63cdc5bf16415a1903bbeaeba635c0b0fb8
Mayza <mayzaumbelina@gmail.com>
2026-06-24 15:24:22 -0400

    fix: remove importação circular de profile no models/__init__

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,6 +2,4 @@ from app.models.user import User
 from app.models.album import Album
 from app.models.photo import Photo
 
-from app.routers import auth, albums, photos, profile
-
-__all__ = ["auth", "albums", "photos", "profile"]
+__all__ = ["User", "Album", "Photo"]
